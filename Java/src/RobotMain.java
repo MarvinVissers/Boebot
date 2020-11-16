fe508d28fe9c5b2d8aef78482b3f5b6a30e2cfbe
MarvinVissers <vissers2000@gmail.com>
2020-11-16 10:29:02 +0100

    Boebot java project aangemaakt in plaats van normaal project

--- Java/src/RobotMain.java
+++ Java/src/RobotMain.java
@@ -6,7 +6,6 @@ public class RobotMain {
     public static void main(String[] args) {
 
         boolean state = true;
-        BoeBot.setMode(0, PinMode.Output);
 
         while (true) {
             state = !state;
