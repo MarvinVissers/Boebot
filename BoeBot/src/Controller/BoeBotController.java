2910b92ae56e92054da4a967233f0ab4bb941a25
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-12-11 15:58:31 +0100

    A star functionaliteit

--- BoeBot/src/Controller/BoeBotController.java
+++ BoeBot/src/Controller/BoeBotController.java
@@ -1,42 +1,48 @@
 package Controller;
 
 import Model.Node;
+import Model.Obstacle;
 import TI.BoeBot;
 
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BoeBotController {
     
-    public void KnipperLinks(){
-        BoeBot.digitalWrite(15, true);
-        BoeBot.wait(200);
-        BoeBot.digitalWrite(15, false);
-        BoeBot.wait(200);
-    }
+//    public void KnipperLinks(){
+//        BoeBot.digitalWrite(15, true);
+//        BoeBot.wait(200);
+//        BoeBot.digitalWrite(15, false);
+//        BoeBot.wait(200);
+//    }
+//
+//    public void KnipperRechts(){
+//        BoeBot.digitalWrite(2, true);
+//        BoeBot.wait(200);
+//        BoeBot.digitalWrite(2, false);
+//        BoeBot.wait(200);
+//    }
+//
+//    public void ObstacleDetect(){
+//        BoeBot.freqOut(0,1000,1000);
+//    }
 
-    public void KnipperRechts(){
-        BoeBot.digitalWrite(2, true);
-        BoeBot.wait(200);
-        BoeBot.digitalWrite(2, false);
-        BoeBot.wait(200);
-    }
-
-    public void ObstacleDetect(){
-        BoeBot.freqOut(0,1000,1000);
-    }
-
-    public void Astar() {
+    public ArrayList<int[]> Astar( ArrayList<int[]> obstacleCoordinates) throws UnknownHostException {
+        ArrayList<int[]> Cordinaten = new ArrayList<>();
+       // System.out.println(Arrays.deepToString(obstacleCoordinates.toArray()));
         Node initialNode = new Node(0, 0);
-        Node finalNode = new Node(2, 5);
-        int rows = 6;
-        int cols = 7;
+        Node finalNode = new Node(6, 5);
+        int rows = 10;
+        int cols = 10;
         AStar aStar = new AStar(rows, cols, initialNode, finalNode);
         //blocksArray = new int[][]{{1, 3}, {2, 3}, {3, 3}};
-        aStar.setBlocks(//iets);
+        aStar.setBlocks(obstacleCoordinates);
         List<Node> path = aStar.findPath();
         for (Node node : path) {
-            System.out.println(node);
+            Cordinaten.add(new int[]{node.getRow(), node.getCol()});
         }
+        return Cordinaten;
     }
 }
--- BoeBot/src/RobotMain.java
+++ BoeBot/src/RobotMain.java
@@ -6,32 +6,53 @@ import Model.Obstacle;
 import Model.Log;
 
 import TI.BoeBot;
+import TI.Servo;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class RobotMain {
+
+    static int AnalogPin1 = 0;  // Rechts
+    static int AnalogPin2 = 1;  // Midde
+    static int AnalogPin3 = 2;  // Links
+
+    static Servo Slink = new Servo(12);
+    static Servo Srechts = new Servo(13);
 
     public static void main(String[] args) throws UnknownHostException {
         // Creating instances of the needed classes
         ObstacleController obstacleCtrl = new ObstacleController("obstacle");
         LogController logCtrl = new LogController("log");
 
+
+
         // Filling the obstacle list
         ArrayList<Obstacle> obstacles = new ArrayList(obstacleCtrl.get());
         // Filling an array with the obstacle coordinates
         ArrayList<int[]> obstacleCoordinates = new ArrayList(obstacleCtrl.createObstacleList(obstacles));
-        System.out.println(obstacleCoordinates.size());
         Log log = new Log(null, "Opstarten Boebot");
         logCtrl.post(log);
         BoeBotController BoeController = new BoeBotController();
-        BoeController.Astar();
-
-
+        ArrayList<int[]> Route = BoeController.Astar(obstacleCoordinates);
         while (true) {
-            BoeController.KnipperLinks();
-            BoeController.KnipperRechts();
-            BoeController.ObstacleDetect();
+           // System.out.println(Arrays.deepToString(BoeController.Astar(obstacleCoordinates).toArray()));
+            //BoeController.Astar(obstacleCoordinates);
+            BoeBot.wait(1000);
+            for (int i = 0; i < Route.size(); i++) {
+                //print de cordinaten
+               System.out.println(Arrays.toString(Route.get(i)));
+                BoeBot.wait(1000);
+                //cordinaten naar een string.
+                StringBuilder strNum = new StringBuilder();
+                for (int num : Route.get(i))
+                {
+                    strNum.append(num);
+                }
+                int finalInt = Integer.parseInt(strNum.toString());
+                System.out.println(finalInt);
+            }
         }
     }
 }
